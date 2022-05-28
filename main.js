f61a683d28195be07e9fb2707ab3b05b251055ac
Hector Haro H <hector8.haro@gmail.com>
2022-05-28 00:11:45 -0500

    add conditional to error message

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 
 const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=2&api_key=67f4aec6-1e09-4a9c-b45f-1228ac4de068'
-
 const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites?limit=2&api_key=67f4aec6-1e09-4a9c-b45f-1228ac4de068'
 
+const spanError = document.getElementById('error')
 
 
 async function loadRandomDog() {
     const res = await fetch(API_URL_RANDOM)
     const data = await res.json()
-    const img1 = document.getElementById('img1')
-    const img2 = document.getElementById('img2')
-    
-    img1.src = data[0].url
-    img2.src = data[1].url
+
+    if(res.status !== 200) {
+        spanError.innerHTML = 'Hubo un error' + res.status
+    }else {
+        const img1 = document.getElementById('img1')
+        const img2 = document.getElementById('img2')
+        
+        img1.src = data[0].url
+        img2.src = data[1].url
+    }
+   
 }
 
 async function loadFavoritesDog() {
     const res = await fetch(API_URL_FAVORITES)
     const data = await res.json()
-    
+    if(res.status !== 200) {
+        spanError.innerHTML = 'Hubo un error: ' + res.status
+    }
     
 }
 
